4b9388d2db8d090497ad47bacf84c43e1e8890c2
terraeel <terraeel@protonmail.com>
2018-02-11 00:08:28 +0100

    Added python3.6

--- data.py
+++ data.py
@@ -6,24 +6,8 @@ import sqlite3
 import time
 
 
-def get_dir_size(path):
-    total = 0
-    for entry in os.scandir(path):
-        try:
-            is_dir = entry.is_dir(follow_symlinks=False)
-        except OSError as error:
-            print('Error calling is_dir():', error, file= sys.stderr)
-            continue
-        if is_dir:
-            total += get_dir_size(entry.path)
-        else:
-            try:
-                total += entry.stat(follow_symlinks=False).st_size
-            except OSError as error:
-                print('Error calling stat():', error, file= sys.stderr)
-    return total
-
-#print(client.df()["Volumes"][2]["UsageData"]["Size"])
+def get_dir_size(client):
+    print(client.df()["Volumes"][2]["UsageData"]["Size"])
 
 def publish_data():
     conn = sqlite3.connect('volumes.db')
