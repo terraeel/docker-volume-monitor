7f860b20db80c96c60ddec235673aabdb3c11e06
Peder Wiig Aalen <peder.wiig@gmail.com>
2018-02-11 08:49:10 +0100

    Henter Json riktig

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+import sqlite3
 
+from flask import Flask, render_template, json
+from volum import Volum
 app = Flask(__name__)
+
+
+def get_volumes():
+    with sqlite3.connect('volumes.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM volumes")
+        all_volumes = cursor.fetchall()
+        return all_volumes
+
+
+@app.route('/api/volumes', methods=['GET'])
+def collection():
+    volumes = get_volumes()
+    return json.dumps(volumes)
 
 
 @app.route('/')
 def index():
-
-    name = "peer"
-    return render_template('index.html', name = name)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ volum.py
@@ -0,0 +1,4 @@
+class Volum:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
