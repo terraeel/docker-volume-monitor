5ed9f5b46f977d6aa2ac8ae4ae8b257dae06d911
Peder Wiig Aalen <peder.wiig@gmail.com>
2018-02-11 18:53:05 +0100

    litt refaktor

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-from dataAccess import dbAccess
+from db.dataAccess import dbAccess
 from flask import Flask, render_template, json
 
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/api/volumes', methods=['GET'])
 def collection():
-
     volumes = dbAccess.get_all_volumes()
     return json.dumps(volumes)
 
 
 @app.route('/api/volumes/<string:name>', methods=['GET'])
 def collection_name(name):
-
     volume = dbAccess.get_volume_by_name(name)
     return json.dumps(volume)
 
 
-@app.route('/')
-def index():
-
-    return render_template('index.html')
-
-
 if __name__ == '__main__':
-
     app.run()
