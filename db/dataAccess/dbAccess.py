2fd55734e653e34b55f5815624050da10c22d1fd
terraeel <terraeel@protonmail.com>
2018-02-11 19:06:52 +0100

    Change project structure

--- db/dataAccess/dbAccess.py
+++ db/dataAccess/dbAccess.py
@@ -1,10 +1,11 @@
-from db import data
 import sqlite3
+import docker
+import time
 
 
 def get_all_volumes():
 
-    data.publish_data()
+    # publish_data()
     with sqlite3.connect('volumes.db') as connection:
         cursor = connection.cursor()
         cursor.execute('''
@@ -21,10 +22,30 @@ def get_all_volumes():
 
 def get_volume_by_name(volume_name):
 
-    data.publish_data()
+    # publish_data()
     with sqlite3.connect('volumes.db') as connection:
         cursor = connection.cursor()
         cursor.execute('''select * from volumes WHERE name = ? ORDER BY DATETIME''', (volume_name,))
         all_volumes = cursor.fetchall()
         return all_volumes
 
+
+def publish_data():
+
+    conn = sqlite3.connect('volumes.db')
+    c = conn.cursor()
+    client = docker.from_env()
+    volumes = client.df()["Volumes"]
+    for volume in volumes:
+        name = volume["Name"]
+        size = volume["UsageData"]["Size"]
+        c.execute("INSERT INTO volumes (name, size) VALUES (?,?)", (name, size))
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+
+    while True:
+        publish_data()
+        time.sleep(30)
