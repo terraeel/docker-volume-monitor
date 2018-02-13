1dabe6ebd11e41f9eab464114fe9b318adbcb337
terraeel <terraeel@protonmail.com>
2018-02-13 15:20:52 +0100

    Fixed CORS

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from db.dataAccess import dbAccess
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, jsonify, request, json
+import requests
+
+app = Flask(__name__, static_folder="./static", template_folder="./templates")
 
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', defaults={'path': ''})
+def index(path):
+    if app.debug:
+        print("Debug")
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
 
 
 @app.route('/api/volumes', methods=['GET', 'POST'])
 def collection():
     if request.method == 'GET':
         volumes = dbAccess.get_all_volumes_current_value()
-        return json.dumps(volumes)
+        return jsonify(volumes)
     elif request.method == 'POST':
         dbAccess.publish_data()
         return 'Success'
@@ -27,4 +30,4 @@ def collection_name(name):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', threaded=True, port=5000)
